2b388b9f89c61810ecc764f9944de25f967683f7
teddy1565 <teddy1565@gmail.com>
2021-10-16 22:19:40 +0800

    clean code

--- src/middlewares/passport.js
+++ src/middlewares/passport.js
@@ -16,27 +16,33 @@ passport.use(new LocalStrategy({
     //改用email當帳號
     usernameField: 'email',
     passwordField:'password'
-    },async(email, password, callback)=>{
-        const user = await UserService.getUser(email);
-        if(!user) {
-            return callback({status:404,message:'沒有該用戶'},false)
-        }
-        if(password !== user.password){
-            return callback({status:404,message:'密碼錯誤!'},false)
+    },async (email, password, callback) => {
+        let user;
+        try {
+            user = await UserService.getUser(email);
+            if(!user) {
+                return callback({status:404,message:'沒有該用戶'},false)
+            }
+            if(password !== user.password){
+                return callback({status:404,message:'密碼錯誤!'},false)
+            }
+        } catch(error) {
+            console.log(error);
+            return callback({status:500,message:"Server Error"},false);
         }
         return callback(null,user);
     }
 ));
 
-passport.use(new jwtStrategy(options,(payload,callback)=>{
+passport.use(new jwtStrategy(options,(payload,callback) => {
     const status = calcExpireTime(payload);
     if(!status){
-        return callback({status:404, message:'Token 時間到期，請重新登入!'})
+        return callback({status:401, message:'Token 時間到期，請重新登入!'}); //http code 404 not applicable
     }
     return callback(null, payload)
 }))
 
-const calcExpireTime = (payload)=>{
+const calcExpireTime = (payload) => {
     const {expireTime}=payload;
     const currentTime = new Date().getTime();
 
